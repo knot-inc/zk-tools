4f8a1b5090f5d0f4e6f5d59c8d2eb2c9a6481a4a
Tomoaki Imai <tomoima525@gmail.com>
2022-07-20 12:41:17 -0700

    compile

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,6 @@
 import { buildPoseidon, buildPoseidonOpt, buildPoseidonReference } from 'circomlibjs';
 import { createPoseidonHash } from './poseidon';
 import { encodeStringToBigInt } from './utils';
-export { buildPoseidon, buildPoseidonOpt, buildPoseidonReference, createPoseidonHash, encodeStringToBigInt, };
+import { generateGroth16Proof } from './generateGroth16Proof';
+import { packToSolidityProof } from './packToSolidityProof';
+export { buildPoseidon, buildPoseidonOpt, buildPoseidonReference, createPoseidonHash, encodeStringToBigInt, generateGroth16Proof, packToSolidityProof, };
